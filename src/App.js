ba3de3998694bec53a32b0addb7f5d6d48a72657
Love to code my life <47113916+vaishnavan@users.noreply.github.com>
2021-06-17 18:24:49 +0530

    musicFinal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import ReactJkMusicPlayer from 'react-jinke-music-player'
+import 'react-jinke-music-player/assets/index.css'
+import { mySongs } from "./songs";
+import {Icon} from 'semantic-ui-react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  
+  constructor(props) {
+    super(props)
+    this.audioInstance = null
+    this.state = {
+       audioList:[],
+       show:false
+    }
+  }
+  
+  componentDidMount(){
+    this.setState({
+      audioList:mySongs
+    })
+  }
+  handlePlay = () => {
+    this.setState({
+      show:!this.state.show
+    })
+  }
+
+  
+  // useEffect(() => {
+  //   setAudioList(mySongs);
+  // }, [])
+
+  render(){
+    const {show} = this.state;
+    return (
+      <div className="App">
+        <h3>Music Player</h3> 
+        <div className="music_functionality">
+          <div className="icon_func">
+            <Icon onClick={() => this.audioInstance.playPrev()} name="step backward" size="large" />
+          </div>
+          <div onClick={this.handlePlay} className="icon_func">
+            {show ? <Icon onClick={() => this.audioInstance.pause()} name="pause" size="large" /> : <Icon  onClick={() => this.audioInstance.play()} name="play" size="large" />}
+          </div>
+          <div className="icon_func">
+            <Icon onClick={() => this.audioInstance.playNext()} name="step forward" size="large" />
+          </div>
+        </div>
+        <div className="listsongs">
+          <ol> 
+            {this.state.audioList.map(el=><li>{el.name}</li>)}
+          </ol>
+        </div>
+       <ReactJkMusicPlayer 
+          onAudioVolumeChange="true"
+          onAudioPlay="false"
+          showMediaSession="true"
+          getAudioInstance={(instance) => {
+              this.audioInstance = instance
+            }}
+       audioLists={this.state.audioList} />
+       
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/songs.js
@@ -0,0 +1,51 @@
+export const mySongs = [{
+        name: 'Aagasam',
+        singer: 'G.V. Prakash Kumar, christin jos, Govind',
+        cover: "https://i.scdn.co/image/ab67616d0000b273f06fc429c88fd58835de00ec",
+        musicSrc: 'https://res.cloudinary.com/dbwhp6cds/video/upload/v1623912255/audio/Aagasam-MassTamilan.io_b7e5e2.mp3'
+    },
+    {
+        name: 'Marandhaye',
+        singer: 'D.Imman',
+        cover: "http://a10.gaanacdn.com/images/song/31/30101731/crop_480x480_1587479599.jpg",
+        musicSrc: 'https://res.cloudinary.com/dbwhp6cds/video/upload/v1623913583/audio/Marandhaye-MassTamilan.io_cgufr1.mp3'
+    }, {
+        name: 'Yaaraiyum-Ivlo-Azhaga',
+        singer: 'Simbu, Mervin Solomon',
+        cover: "https://a10.gaanacdn.com/gn_img/song/10q3Zj1352/q3ZpQqYAb5/size_xxl_1617267392.webp",
+        musicSrc: 'https://res.cloudinary.com/dbwhp6cds/video/upload/v1623913971/audio/Yaaraiyum-Ivlo-Azhaga-MassTamilan.io_vgwbhy.mp3'
+    }, {
+        name: 'Kaattu-Payale',
+        singer: 'Dhee',
+        cover: "https://a10.gaanacdn.com/gn_img/song/VdNW0JMKo5/NW0xzokEWo/size_xxl_1595481509.webp",
+        musicSrc: 'https://res.cloudinary.com/dbwhp6cds/video/upload/v1623914283/audio/Kaattu-Payale-MassTamilan.io_e3pmlr.mp3'
+    }, {
+        name: 'Nanbiye',
+        singer: 'Anirudh Ravichander',
+        cover: "http://a10.gaanacdn.com/images/song/0/29695600/crop_480x480_1582903033.jpg",
+        musicSrc: 'https://res.cloudinary.com/dbwhp6cds/video/upload/v1623924960/audio/Nanbiye-MassTamilan.io_bxcn4d.mp3'
+    }, {
+        name: 'Naalu-Nimisham',
+        singer: 'Krishnaraji',
+        cover: "https://i.scdn.co/image/ab67616d0000b273f06fc429c88fd58835de00ec",
+        musicSrc: 'https://res.cloudinary.com/dbwhp6cds/video/upload/v1623925012/audio/Naalu-Nimisham-MassTamilan.io_lqvkrp.mp3'
+    }, {
+        name: 'Chitti (Jathi Ratnalu)',
+        singer: 'Radhan',
+        cover: "https://pbs.twimg.com/media/ErnAYj7UcAIQT9o.jpg",
+        musicSrc: 'https://res.cloudinary.com/dbwhp6cds/video/upload/v1623925679/audio/Chitti_Jathi_Ratnalu_-_Telugu-wap.in_tvk2xy.mp3'
+    }
+    , {
+        name: 'He is So Cute',
+        singer: 'Madhu priya',
+        cover: "https://lh3.googleusercontent.com/proxy/EzzsalLHuK24pPskeEH208ChkR7UKEQ0hKNHWvAfrt9Gc1GmMvM0PHESbx74l2739rWn7cEQB0aCwD1W9tUNtz0yf_U58au02x2DDMK2",
+        musicSrc: 'https://res.cloudinary.com/dbwhp6cds/video/upload/v1623925680/audio/He_is_So_Cute_Mp3_Song_From_Sarileru_Neekevvaru_Movie_dmw81l.mp3'
+    }
+    , {
+        name: 'Kanna-Veesi-Kanna-Veesi',
+        singer: 'Rahul and Srinisha Jayaseelan',
+        cover: "https://www.tamil2lyrics.com/wp-content/uploads/2020/01/Kadhal-Ondru-Kanden.jpg",
+        musicSrc: 'https://res.cloudinary.com/dbwhp6cds/video/upload/v1623925678/audio/Kanna-Veesi-Kanna-Veesi-MassTamilan.io_onmdy9.mp3'
+    }
+
+]
